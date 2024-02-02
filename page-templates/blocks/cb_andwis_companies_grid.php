a080e36415f1ebe55463720dba6d22e7de120716
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-02-02 14:35:24 +0000

    andwis companies grid

---
+++ page-templates/blocks/cb_andwis_companies_grid.php
@@ -0,0 +1,21 @@
+<section class="andwis_companies_grid py-4">
+    <div class="container-xl">
+        <h2 class="h3 text-green-900 dot mb-5">Our Andwis Group Companies</h2>
+        <div class="andwis_companies_grid__grid mb-4">
+            <?php
+        while (have_rows('andwis', 'options')) {
+            the_row();
+            ?>
+            <div class="andwis_companies_grid__logo">
+                <img src="<?= wp_get_attachment_image_url(get_sub_field('logo'), 'large')?>"
+                    alt="<?=get_sub_field('name')?>">
+            </div>
+            <?php
+        }
+            ?>
+        </div>
+        <div class="text-center">
+            <a href="/our-companies/" class="btn btn-primary">More about andwis</a>
+        </div>
+    </div>
+</section>
